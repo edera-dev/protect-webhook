e86c7a3b58321bafb34834370cb4ba2deb1d6b09
James Petersen <jpetersenames@gmail.com>
2024-12-06 13:07:01 -0500

    add chart
    Signed-off-by: James Petersen <jpetersenames@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use anyhow::Result;
 use base64::prelude::*;
+use log::{debug, info};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
-use std::collections::HashMap;
+use std::env;
 use warp::Filter;
 
 const RUNTIME_CLASS: &str = "edera";
@@ -15,17 +16,6 @@ struct AdmissionReview {
 #[derive(Deserialize, Debug, Clone)]
 struct AdmissionRequest {
     uid: String,
-    object: Pod,
-}
-
-#[derive(Deserialize, Debug, Clone)]
-struct Pod {
-    metadata: Option<Metadata>,
-}
-
-#[derive(Deserialize, Debug, Default, Clone)]
-struct Metadata {
-    labels: Option<std::collections::HashMap<String, String>>,
 }
 
 #[derive(Serialize, Debug)]
@@ -47,89 +37,68 @@ struct Response {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    println!("starting server");
-    let mutate = warp::post()
-        .and(warp::path("mutate"))
-        .and(warp::body::json())
-        .map(|admission_review: AdmissionReview| {
-            let response = mutate(admission_review).unwrap();
-            println!("mutating {:#?}", response);
-            warp::reply::json(&response)
-        });
+    env_logger::init();
 
-    println!("listening on 8443");
-    warp::serve(mutate)
+    let certs_dir = env::var("WEBHOOK_CERTS_DIR").unwrap_or("/certs".to_string());
+    let crt_file = env::var("WEBHOOK_CRT_FILE").unwrap_or("tls.crt".to_string());
+    let key_file = env::var("WEBHOOK_KEY_FILE").unwrap_or("tls.key".to_string());
+    info!("configured certs directory to: {}", certs_dir);
+
+    let routes = mutate().or(livez()).or(healthz());
+
+    info!("listening on 8443");
+    warp::serve(routes)
         .tls()
-        .cert_path("/certs/tls.crt")
-        .key_path("/certs/tls.key")
+        .cert_path(format!("{}/{}", certs_dir, crt_file))
+        .key_path(format!("{}/{}", certs_dir, key_file))
         .run(([0, 0, 0, 0], 8443))
         .await;
 
     Ok(())
 }
 
-fn should_mutate(review: AdmissionReview) -> Result<bool> {
-    let request = review.request.unwrap();
-    let pod = request.object;
-    let metadata = pod.metadata.unwrap_or_default();
-    let labels = metadata.clone().labels.unwrap_or_default();
-
-    println!("Received request for {:#?}", metadata.clone());
-
-    let selectors = HashMap::from([
-        ("actions-ephemeral-runner", "true"),
-        ("actions.github.com/scale-set-name", "arc-runner-set-edera"),
-    ]);
-
-    let mut matched = 0;
-    for (key, value) in selectors.clone() {
-        let Some(label_value) = labels.get(key) else {
-            continue;
-        };
-
-        if label_value.to_lowercase() == value {
-            matched += 1;
-        }
-    }
-    Ok(matched == selectors.len())
+fn healthz() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::get().and(warp::path("healthz")).map(|| {
+        debug!("GET /healthz");
+        warp::reply()
+    })
 }
 
-fn mutate(review: AdmissionReview) -> Result<AdmissionReviewResponse> {
+fn livez() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::get().and(warp::path("livez")).map(|| {
+        debug!("GET /livez");
+        warp::reply()
+    })
+}
+
+fn mutate() -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
+    warp::post()
+        .and(warp::path("mutate"))
+        .and(warp::body::json())
+        .map(|admission_review: AdmissionReview| {
+            let response = mutate_internal(admission_review).unwrap();
+            info!("mutating {:?}", response);
+            warp::reply::json(&response)
+        })
+}
+
+fn mutate_internal(review: AdmissionReview) -> Result<AdmissionReviewResponse> {
     let request = review.request.clone().unwrap();
-    if should_mutate(review)? {
-        // Create a JSONPatch to add runtimeClassName
-        let patch = json!([{
-            "op": "add",
-            "path": "/spec/runtimeClassName",
-            "value": RUNTIME_CLASS
-        },{
-            "op": "add",
-            "path": "/metadata/annotations/dev.edera~1kernel_verbose",
-            "value": "true"
-        }]);
-        let patch_base64 = BASE64_STANDARD.encode(serde_json::to_string(&patch).unwrap());
+    let patch = json!([{
+        "op": "add",
+        "path": "/spec/runtimeClassName",
+        "value": RUNTIME_CLASS
+    }]);
+    let patch_base64 = BASE64_STANDARD.encode(serde_json::to_string(&patch).unwrap());
 
-        return Ok(AdmissionReviewResponse {
-            api_version: "admission.k8s.io/v1".to_string(),
-            kind: "AdmissionReview".to_string(),
-            response: Some(Response {
-                uid: request.uid,
-                allowed: true,
-                patch_type: Some("JSONPatch".to_string()),
-                patch: Some(patch_base64),
-            }),
-        });
-    }
-
-    // If no label matches, allow the pod without changes
     Ok(AdmissionReviewResponse {
         api_version: "admission.k8s.io/v1".to_string(),
         kind: "AdmissionReview".to_string(),
         response: Some(Response {
             uid: request.uid,
             allowed: true,
-            patch_type: None,
-            patch: None,
+            patch_type: Some("JSONPatch".to_string()),
+            patch: Some(patch_base64),
         }),
     })
 }
